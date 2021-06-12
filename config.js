a63b31e20f51a669e8dcf12a88361f6ebae455c6
Marshxan <84200284+Marshxan@users.noreply.github.com>
2021-06-12 03:01:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    // Your Discord bot token
+    token: "TOKEN_HERE",
+
+    // The bot game : Playing to...
+    game: "Marshy Is Cool",
+
+    /**
+     * The messages that the bot will send 
+     * {user} will be automatically replaced by User#0000
+     * {guild} will be automatically replaced by the server name
+     */
+    sentences: {
+        logs: "{user} was banned from {guild} because it's a selfbot!",
+        banned: "You were banned from {guild} because we have detected that you are a selfbot!",
+        whitelist: "Sorry, this server is not included in the whitelist!"
+    },
+
+    // The whitelist allows you to limit the bot to certain servers 
+    whitelist: {
+        enabled: false, // Whether the whitelist is enabled
+        guilds: [] // Servers that are whitelisted
+    },
+
+    // The ID of the channel where the log messages will be sent (for admins)
+    logs: "CHANNEL_ID"
+    
+}
+
